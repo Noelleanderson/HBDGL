121e6e23bebccd0e675e3d177576ce68af952849
Noelleanderson <noellemarieanderson@gmail.com>
2017-09-18 15:37:14 -0700

    added package installs

--- GL_musicapp.R
+++ GL_musicapp.R
@@ -2,10 +2,12 @@
 # Graham's birthday plot!
 # Run the application by clicking the 'Run App' button above.
 #
+install.packages(c("shiny","dplyr","srtreamgraph","randomColor"))
 
 library(shiny)
+library(dplyr)
 library(streamgraph)
-library(tidyverse)
+library(randomColor)
 
 gl_music <- read_delim("gl.formatted.tab","\t", escape_double = F, col_names = T,trim_ws = TRUE)
 numofartists <- gl_music %>% group_by(artist) %>% tally %>% nrow()
