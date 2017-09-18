d9e2458b995dc49512d1dcf85b1f8a8d79875603
Noelleanderson <noellemarieanderson@gmail.com>
2017-09-18 16:34:55 -0700

    added setup file

---
+++ GL_musicapp.setup.R
@@ -0,0 +1,13 @@
+# Run before GL_musicapp.R
+
+install.packages(c("shiny","dplyr","srtreamgraph","randomColor"))
+
+library(shiny)
+library(dplyr)
+library(streamgraph)
+library(randomColor)
+
+gl_music <- read_delim("/Users/Noelle/GL/gl.formatted.tab","\t", escape_double = F, col_names = T,trim_ws = TRUE)
+numofartists <- gl_music %>% group_by(artist) %>% tally %>% nrow()
+#COLS <- rainbow(numofartists)
+c<-randomColor(numofartists)
